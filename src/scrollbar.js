c3c15393b739458d5ea3a284537a60547e3878e3
Alex Davey <alexdavey0@googlemail.com>
2012-05-16 17:40:07 +0100

    Automated tests, refactored files

---
+++ src/scrollbar.js
@@ -0,0 +1,53 @@
+require(['plugin', 'events', 'viewport', 'text', 'settings'], function(plugin, events, viewport, text, settings) {
+
+	"use strict";
+
+	function render(ctx, model) {
+		if (text.source.length <= viewport.height + 1) return;
+
+		var oldLineCap = ctx.lineCap,
+			x = model.x,
+			y = model.bar.offset;
+	
+		ctx.lineCap = 'round';
+		ctx.strokeStyle = 'rgba(181, 181, 181, 0.5)';
+		ctx.lineWidth = model.width;
+
+		ctx.beginPath();
+
+		ctx.moveTo(x, y);
+		ctx.lineTo(x, y + model.bar.height);
+
+		ctx.stroke();
+
+		ctx.lineCap = oldLineCap;
+	}
+	
+	var model = plugin.element({
+		visible : true,
+		clickable : true,
+		floating : true,
+		width : 8,
+		render : render,
+		x : settings.width - 8,
+		y : 0,
+		z : 10,
+		bar :  {
+			height : 0,
+			offset : 0
+		}
+	});
+
+	events.subscribe('moveViewport|textModified', function() {
+		var canvasHeight = settings.height,
+			textLength = text.source.length;
+
+		model.bar.height = (viewport.height / textLength) * canvasHeight - 16;
+		model.bar.offset = (canvasHeight / textLength) * viewport.startRow + 8;
+	});
+
+	// model.click(function(e) {
+	// 	viewport.shiftTo('start', (e.y / settings.height) * text.source.length);
+	// });
+
+});
--- tests/test.sh
+++ tests/test.sh
@@ -4,19 +4,65 @@
 IFS=$','
 
 # Arguments required for every test
-args=("../js/lib/require.js"
-	  "../js/lib/underscore.js"
-	  "../js/utility.js"
+args=("../src/lib/require.js"
+	  "../src/lib/underscore.js"
+	  "../src/utility.js"
 	  "test.js")
 
-# if $1=="*"
-# then
-# 	args+=$(ls *.js) + $("../js/*.js" + ls -r ../js/*.js)
-# fi
+dependencies=()
+
+function contains {
+	for i in $0
+	do
+		if i == $1
+			then return true
+		fi
+	done
+
+	return false
+}
+
+function addDependencies {
+	# Get the dependencies of the file using sed,
+	# 'require(['trie', 'canvas', 'test'], function...' becomes
+	# 'trie,canvas,test' and is then split at the comma
+	dependencies=$0 | sed 's/require(\[\(.*\)\].*/\1/' | sed s/\'//g | sed 's/ *\, */,/g' # TODO: split at comma! + global recursive search
+
+	# Add all of the dependencies
+	for j in dependencies
+	do
+		# Only add the dependency if it has not already been added
+		if ! contains dependencies j
+			then
+
+			# Add the dependencies of that file
+			addDependencies j
+
+			# Add name of dependencies to list of loaded dependencies
+			$dependencies=(${dependencies[@]} ${j})
+
+			echo "Adding ../src/${j}.js as a dependency"
+
+			# Add that file
+			args=(${args[@]} "../src/${j}.js")
+
+		fi
+	done
+}
+
+# Loop over all of the arguments to the test script
+for i in $*
+do
+	addDependencies i
+
+	# Add the source file itself and the test file
+	args=("${args[@]}" "../src/${i}.js" "${i}.js")
+done
+
 
 # Run the test with the new arguments, concatenating
 # them with a comma and prepending the js_files flag
-exec gjstest "--js_files=${args[*]},$*"
+exec gjstest "--js_files=${args[*]}"
 
 # Reset the delimiter for safety
 unset IFS
